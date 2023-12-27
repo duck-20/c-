584be8640374fe59b2cc6ba1cb3cf5b57a773c72
Ayush Pakhrin <pakhrinayush56@gmail.com>
2023-12-27 21:16:44 +0545

    added employee using classes and obj

---
+++ Employees/Employees/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.Models
+{
+    public class Employee
+    {
+        public string name;
+        public int age;
+        public string department;
+
+        public Employee(string name,int age, string department) {
+            
+            this.name = name;
+            this.age = age; 
+            this.department = department;
+            Console.WriteLine("Employee Details:");
+            Console.WriteLine($"Name:{name}");
+            Console.WriteLine($"Age:{age}");
+            Console.WriteLine($"Department:{department}");
+        }
+    }
+}
---
+++ Employees/Employees/Program.cs
@@ -0,0 +1,21 @@
+﻿using Employees.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee=new Employee("Ayush",21,"BCA");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}

4ca151fcacc845366395dc0f1e6f3f6e6ea8f270
Ayush Pakhrin <pakhrinayush56@gmail.com>
2024-01-31 22:14:28 +0545

    added async

---
+++ Async/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string food = "MoMo";
+            Waiter();
+            Customer(food);
+            Cook(food);
+            Console.ReadKey();
+        }
+        static async Task Waiter()
+        {
+           Console.WriteLine($"Waiter: What's your order?");
+           await Task.Delay(2000);
+           Console.WriteLine($"Waiter: Order Taken!");
+        }
+        static void Customer(string foodName)
+        {
+            Console.WriteLine($"Customer: I would like to have a {foodName}.");
+        }
+        static async Task Cook(string foodName)
+        {
+            await Task.Delay(4000);
+            Console.WriteLine($"Cook: {foodName} is being prepared.");
+            await Task.Delay(2000);
+            Console.WriteLine($"Cook: {foodName} is ready.");
+        }
+    }
+}
